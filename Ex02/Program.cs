9c3682f880b32b0ebb7340c0803dec5abbd21dad
Tony Cotes <cotesgarciatony@gmail.com>
2024-10-18 12:28:15 +0200

    Ex02

---
+++ Ex02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+namespace Ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t1 = 25;
+            int t2 = 25;
+            int t3 = 25;
+            bool iguals;
+            iguals = t1==t2&&t2==t3;
+            if (iguals)
+            {
+                Console.WriteLine("Les tres temperatures són iguals.");
+            }
+            else
+            {
+                Console.WriteLine("Les temperatures no són iguals.");
+            }
+        }
+    }
+}
