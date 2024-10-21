f9d8a43032116e8d9dcf6813bec845a5d7812455
Tony Cotes <cotesgarciatony@gmail.com>
2024-10-21 22:56:39 +0200

    Ex09

---
+++ Ex09/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Ex09
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Definició de variables
+            double notaExamen, notaPractiques;
+            string notaQualitativa;
+
+            //Dades d'entrada
+            Console.Write("Introdueix la nota de l'examen: ");
+            notaExamen = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Introdueix la nota de les pràctiques: ");
+            notaPractiques = Convert.ToDouble(Console.ReadLine());
+
+            //Algorisme i Dades de sortida
+            notaQualitativa = NotaQualitativa(notaExamen, notaPractiques);
+            Console.Write($"La nota qualitativa és: {notaQualitativa}");
+        }
+        public static string NotaQualitativa(double notaExamen, double notaPractiques)
+        {
+            string nota;
+            double notaGlobal;
+            notaGlobal = (notaExamen * 0.8) + (notaPractiques * 0.2);
+            if (notaExamen < 3 || notaPractiques < 3)
+            {
+                nota = "Suspens";
+            }
+
+            if (notaGlobal < 5)
+                nota = "Suspens";
+                else if (notaGlobal >= 5 && notaGlobal < 7)
+                    nota = "Aprovat";
+                    else if (notaGlobal >= 7 && notaGlobal < 9)
+                        nota = "Notable";
+                        else
+                            nota = "Excel·lent";
+            return nota;
+        }
+    }
+}
