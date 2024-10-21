7439f4134ab81a3bc59312d7bc9bc2060245bcf6
Tony Cotes <cotesgarciatony@gmail.com>
2024-10-21 22:56:33 +0200

    Ex07

---
+++ Ex07/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Ex07
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            //Definició de variables
+            int velocitat;
+            string sancio;
+
+            //Dades d'entrada
+            Console.Write("Introdueix la velocitat del vehicle: ");
+            velocitat = Convert.ToInt32(Console.ReadLine());
+
+            //Algorisme i Dades de sortida
+            sancio = Sancio(velocitat);
+            Console.Write($"Sanció: {sancio}");
+        }
+        public static string Sancio(int velocitat)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            string multa;
+            if (velocitat >= 80 && velocitat <= 99)
+                multa = "100€ de multa";
+                    else if (velocitat >= 100 && velocitat <= 129)
+                    multa = "300€ de multa";
+                        else if (velocitat >= 130)
+                        multa = "600€ de multa i retirada de carnet";
+                            else
+                            multa = "sense sanció";
+            return multa;
+        }
+    }
+}
