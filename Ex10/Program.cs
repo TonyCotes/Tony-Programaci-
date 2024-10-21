2fc6ebb240382f21e34c2bffbd5e729dd333bbdc
Tony Cotes <cotesgarciatony@gmail.com>
2024-10-21 22:56:45 +0200

    Ex10

---
+++ Ex10/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Ex10
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Definició de variables
+            int any;
+            string resultat;
+            
+            //Dades d'entrada
+            Console.Write("Introdueix un any: ");
+            any = Convert.ToInt32(Console.ReadLine());
+
+            //Algorisme i dades de sortida
+            resultat = Segle(any);
+            Console.Write(resultat);
+        }
+            public static string Segle(int any)
+        {
+            string segle;
+            if (any >= 1701 && any <= 1800)
+                segle = "Segle XVIII";
+                else if (any >= 1801 && any <= 1900)
+                    segle = "Segle XIX";
+                    else if (any >= 1901 && any <= 2000)
+                        segle = "Segle XX";
+                        else if (any >= 2001 && any <= 2100)
+                            segle = "Segle XXI";
+                            else
+                                segle = "Any invàlid";
+            return segle;
+        }
+    }
+}
