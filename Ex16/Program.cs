68e1265cb558c73c58236de73e31403b77a9b756
Tony Cotes <cotesgarciatony@gmail.com>
2024-10-22 09:59:15 +0200

    a

--- Ex16/Program.cs
+++ Ex16/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int any;
+
+            if (EsAnyFestivalHulukulu(any) && EsAnyTraspas(any) && EsAnyFestivalBulukulu(any))
+
+        }
+        static bool EsAnyTraspas(int any)
+        {
+            bool traspas;
+            if (any % 400 == 0 || any % 100 != 0 && any % 4 == 0)
+                traspas = true;
+            else
+                traspas = false;
+            return traspas;
+        }
+        static bool EsAnyFestivalHulukulu(int any)
+        {
+            bool hulu;
+            if (any % 15 == 0)
+                hulu = true;
+            else 
+                hulu = false;
+            return hulu;
+        }
+        static bool EsAnyFestivalBulukulu(int any)
+        {
+            bool bulu;
+            if (any % )
         }
     }
 }
